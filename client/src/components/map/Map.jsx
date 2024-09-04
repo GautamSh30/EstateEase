384b1ed9c910001870f497e999824e75f6d9c829
Gautam Sharma <gautam2586272@gmail.com>
2024-09-04 19:16:17 +0530

    final commit

---
+++ client/src/components/map/Map.jsx
@@ -0,0 +1,29 @@
+import "./map.scss";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import Pin from "../pin/Pin";
+
+function Map({ items }) {
+  return (
+    <MapContainer
+      center={
+        items.length === 1
+          ? [items[0].latitude, items[0].longitude]
+          : [23.3, 80.3]
+      }
+      zoom={items.length == 1 ? 10 : 5}
+      scrollWheelZoom={true}
+      className="map"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {items.map((item) => (
+        <Pin item={item} key={item.id} />
+      ))}
+    </MapContainer>
+  );
+}
+
+export default Map;
